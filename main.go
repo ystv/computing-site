abd6c02e8976a5889890381b87844da75ce69f7f
Liam Burnand <liam.burnand@ystv.co.uk>
2024-01-06 02:08:16 +0000

    Changing from using mux to labstack echo

--- main.go
+++ main.go
@@ -2,14 +2,10 @@ package main
 
 import (
 	"embed"
-	"fmt"
-	"github.com/gorilla/mux"
+	"log"
+
 	"github.com/ystv/computing_site/link"
 	"github.com/ystv/computing_site/team"
-	"github.com/ystv/computing_site/templates"
-	"io/fs"
-	"log"
-	"net/http"
 )
 
 //go:embed public/*
@@ -20,68 +16,30 @@ var (
 	Version = "unknown"
 )
 
-type Web struct {
-	mux  *mux.Router
-	t    *templates.Templater
-	link *link.Link
-	team *[]team.Member
-}
-
 func main() {
-	var err error
-
-	web := Web{
-		mux: mux.NewRouter(),
-	}
-	web.link, err = link.New()
+	link1, err := link.New()
 	if err != nil {
 		log.Printf("failed to get link: %+v\n", err)
 	}
-	web.team, err = team.New()
+	team1, err := team.New()
 	if err != nil {
 		log.Printf("failed to get team: %+v\n", err)
 	}
 
-	assetHandler := http.FileServer(getFileSystem())
-
 	addr := "0.0.0.0:7075"
 
-	web.mux.HandleFunc("/", web.indexPage).Methods("GET")
-	//web.mux.HandleFunc("/ystv.ico", web.faviconHandler)
-	//web.mux.HandleFunc("/stylesheet.css", web.cssHandler)
-	web.mux.PathPrefix("/public/").Handler(http.StripPrefix("/public/", assetHandler))
-	log.Printf("YSTV Computing site: %s, commit: %s, version: %s\n", addr, Commit, Version)
-	log.Fatal(http.ListenAndServe(addr, web.mux))
-}
-
-func (web *Web) indexPage(w http.ResponseWriter, _ *http.Request) {
-	params := &templates.DashboardParams{
-		Link:    web.link,
-		Team:    web.team,
+	router := NewRouter(&RouterConf{
+		Address: addr,
+		Link:    link1,
+		Team:    team1,
 		Commit:  Commit,
 		Version: Version,
-	}
+	})
 
-	err := web.t.RenderTemplate(w, params, "dashboard.tmpl")
+	log.Printf("YSTV Computing site: %s, commit: %s, version: %s\n", addr, Commit, Version)
+
+	err = router.Start()
 	if err != nil {
-		err = fmt.Errorf("failed to render dashboard: %w", err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Fatalf("The web server couldn't be started!\n\n%s\n\nExiting!", err)
 	}
-}
-
-//func (web *Web) faviconHandler(w http.ResponseWriter, r *http.Request) {
-//	http.ServeFile(w, r, "public/ystv.ico")
-//}
-//
-//func (web *Web) cssHandler(w http.ResponseWriter, r *http.Request) {
-//	http.ServeFile(w, r, "public/stylesheet.css")
-//}
-
-func getFileSystem() http.FileSystem {
-	fsys, err := fs.Sub(embeddedFiles, "public")
-	if err != nil {
-		panic(err)
-	}
-
-	return http.FS(fsys)
 }
---
+++ router.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+
+	"github.com/ystv/computing_site/link"
+	"github.com/ystv/computing_site/team"
+	"github.com/ystv/computing_site/templates"
+)
+
+type (
+	Router struct {
+		address   string
+		templater *templates.Templater
+		link      *link.Link
+		team      *[]team.Member
+		commit    string
+		version   string
+		router    *echo.Echo
+	}
+
+	RouterConf struct {
+		Address string
+		Link    *link.Link
+		Team    *[]team.Member
+		Commit  string
+		Version string
+	}
+)
+
+func NewRouter(conf *RouterConf) *Router {
+	r := &Router{
+		address: conf.Address,
+		link:    conf.Link,
+		team:    conf.Team,
+		commit:  conf.Commit,
+		version: conf.Version,
+		router:  echo.New(),
+	}
+	r.router.HideBanner = true
+
+	r.middleware()
+
+	r.loadRoutes()
+
+	return r
+}
+
+func (r *Router) Start() error {
+	r.router.Logger.Error(r.router.Start(r.address))
+	return fmt.Errorf("failed to start router on address %s", r.address)
+}
+
+// middleware initialises web server middleware
+func (r *Router) middleware() {
+	r.router.Pre(middleware.RemoveTrailingSlash())
+	r.router.Use(middleware.Recover())
+	r.router.Use(middleware.BodyLimit("15M"))
+	r.router.Use(middleware.GzipWithConfig(middleware.GzipConfig{
+		Level: 5,
+	}))
+}
+
+func (r *Router) loadRoutes() {
+	r.router.RouteNotFound("/*", func(c echo.Context) error { return c.Redirect(http.StatusFound, "/") })
+
+	assetHandler := http.FileServer(http.FS(echo.MustSubFS(embeddedFiles, "public")))
+
+	r.router.GET("/public/*", echo.WrapHandler(http.StripPrefix("/public/", assetHandler)))
+
+	base := r.router.Group("/")
+
+	base.GET("api/health", func(c echo.Context) error {
+		marshal, err := json.Marshal(struct {
+			Status int `json:"status"`
+		}{
+			Status: http.StatusOK,
+		})
+		if err != nil {
+			log.Println(err)
+			return &echo.HTTPError{
+				Code:     http.StatusBadRequest,
+				Message:  err.Error(),
+				Internal: err,
+			}
+		}
+
+		c.Response().Header().Set("Content-Type", "application/json")
+		return c.JSON(http.StatusOK, marshal)
+	})
+
+	base.GET("", r.HomeFunc)
+}
+
+func (r *Router) HomeFunc(c echo.Context) error {
+	params := &templates.DashboardParams{
+		Link:    r.link,
+		Team:    r.team,
+		Commit:  r.commit,
+		Version: r.version,
+	}
+
+	return r.templater.RenderTemplate(c.Response(), params, "dashboard.tmpl")
+}
